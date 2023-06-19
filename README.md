fb043e4328063be687828da1a3cecb21a3c6bcdd
Mohammed Abdulrazeg <github@mzawi.me>
2023-06-19 13:41:32 +0100

    Initial custom AstraNvim setup

---
+++ README.md
@@ -0,0 +1,18 @@
+# Mo's AstraNvim custom scripts
+
+My user customisations for AstraNvim. To get started, make sure to backup current nvim folder
+
+```sh
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
+```
+
+Grab AstraNvim and these settings
+
+```sh
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+git clone https://github.com/githubmo/astronvim_config.git ~/.config/nvim/lua/user
+nvim
+```
---
+++ init.lua
@@ -0,0 +1,20 @@
+return {
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.nightfox-nvim" },
+    { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+    { import = "astrocommunity.colorscheme.iceberg-vim" },
+    { import = "astrocommunity.colorscheme.nightfox-nvim" },
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.pack.typescript" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity.pack.prisma" },
+    { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.pack.markdown" },
+    { import = "astrocommunity.pack.toml" },
+    { import = "astrocommunity.pack.yaml" },
+    { import = "astrocommunity.pack.docker" },
+    { import = "astrocommunity.pack.terraform" },
+  },
+  colorscheme = "iceberg",
+}
